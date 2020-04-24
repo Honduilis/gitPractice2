26c1a4e0bcba3953b269874f089393589e3b8f33
Honduilis <tiernailis@hotmail.com>
2020-04-24 16:36:56 -0400

    usingGit.java

--- src/day1/allGood.java
+++ src/day1/allGood.java
@@ -1,6 +1,6 @@
 package day1;
 
-public class allGood {
+public class   allGood {
     public static void main(String[] args) {
 
         // I dream about my new Job, God will provide for us in the name of Jesus.
---
+++ src/day1/usingGit.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class usingGit {
+    public static void main(String[] args) {
+        // git add . git commit -m"className fileName"
+
+
+    }
+}
