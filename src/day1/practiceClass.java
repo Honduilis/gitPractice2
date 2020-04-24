c622d38053ac91e5a87eb0f0187d160b5cd68394
Honduilis <tiernailis@hotmail.com>
2020-04-24 15:56:16 -0400

    practiceClass.java

---
+++ src/day1/practiceClass.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class practiceClass {
+    public static void main(String[] args) {
+
+        // git is a version control system to save codes, stated in 2005
+        System.out.println("this is git");
+
+    }
+}
