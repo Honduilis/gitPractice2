762bff4ffc96fa785ed27760145f36117b9406ff
Honduilis <64107457+Honduilis@users.noreply.github.com>
2020-04-24 16:30:11 -0400

    Update allGood.java

--- src/day1/allGood.java
+++ src/day1/allGood.java
@@ -5,6 +5,9 @@ public class allGood {
 
         // I dream about my new Job, God will provide for us in the name of Jesus.
         System.out.println("I like Java");
+        
+        // git was invented in 2005
+  
 
     }
 }
