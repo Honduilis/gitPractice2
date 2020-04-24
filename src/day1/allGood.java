c34a4476e4792162ab11b96d7694c5c38b327f9f
Honduilis <tiernailis@hotmail.com>
2020-04-24 15:04:09 -0400

    first commit.java

---
+++ src/day1/allGood.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class allGood {
+    public static void main(String[] args) {
+
+        // I dream about my new Job, God will provide for us in the name of Jesus.
+        System.out.println("I like Java");
+
+    }
+}
