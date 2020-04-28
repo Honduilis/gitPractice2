f3b3e9e12d39b2949e88c95e62fa6a6381003e6a
Honduilis <tiernailis@hotmail.com>
2020-04-28 15:40:34 -0400

    add HappyBirthday.java

--- src/day1/AddCommand.java
+++
@@ -1,9 +0,0 @@
-package day1;
-
-public class AddCommand {
-    public static void main(String[] args) {
-
-         // git is a version control System
-
-    }
-}
---
+++ src/day1/HappyBirthday.java
@@ -0,0 +1,7 @@
+package day1;
+
+public class HappyBirthday {
+    public static void main(String[] args) {
+        // Happy birthday to all my class mates
+    }
+}
