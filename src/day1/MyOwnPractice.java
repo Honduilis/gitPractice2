3b961d678ea9ec10a84484b0a1e3218671c9ffc7
Honduilis <tiernailis@hotmail.com>
2020-04-28 19:37:54 -0400

    add MyOwnPractice.java

---
+++ src/day1/MyOwnPractice.java
@@ -0,0 +1,7 @@
+package day1;
+
+public class MyOwnPractice {
+    public static void main(String[] args) {
+        System.out.println("it's been very interesting experience learning java");
+    }
+}
