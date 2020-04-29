3ad2db4abef24496503712837e043686f07e884c
Honduilis <64107457+Honduilis@users.noreply.github.com>
2020-04-28 20:38:44 -0400

    Update gitIsFun.java

--- src/day1/gitIsFun.java
+++ src/day1/gitIsFun.java
@@ -3,5 +3,6 @@ package day1;
 public class gitIsFun {
     public static void main(String[] args) {
         System.out.println("git is fun to learn");
+        // will not be easy but not imposible 
     }
 }
